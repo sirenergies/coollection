b0f95731084f5518c3a28e5a1464b394cd4928a3
Potsky <potsky@me.com>
2022-05-18 09:54:41 +0200

    Support PHP8.1

--- src/package/Coollection.php
+++ src/package/Coollection.php
@@ -8,6 +8,7 @@ use Exception;
 use ArrayAccess;
 use JsonSerializable;
 use IteratorAggregate;
+use Traversable;
 use IlluminateAgnostic\Str\Support\Str;
 use IlluminateAgnostic\Arr\Support\Arr;
 use IlluminateAgnostic\Collection\Support\Collection;
@@ -454,7 +455,7 @@ class Coollection implements
      * @param  mixed  $key
      * @return bool
      */
-    public function offsetExists($key)
+    public function offsetExists($key): bool
     {
         return array_key_exists($key, $this->items);
     }
@@ -465,7 +466,7 @@ class Coollection implements
      * @param  mixed  $key
      * @return mixed
      */
-    public function offsetGet($key)
+    public function offsetGet($key): mixed
     {
         if (!isset($this->items[$key])) {
             return null;
@@ -481,7 +482,7 @@ class Coollection implements
      * @param  mixed  $value
      * @return void
      */
-    public function offsetSet($key, $value)
+    public function offsetSet($key, $value): void
     {
         if (is_null($key)) {
             $this->items[] = $value;
@@ -496,7 +497,7 @@ class Coollection implements
      * @param  string  $key
      * @return void
      */
-    public function offsetUnset($key)
+    public function offsetUnset($key): void
     {
         unset($this->items[$key]);
     }
@@ -576,7 +577,7 @@ class Coollection implements
      * which is a value of any type other than a resource.
      * @since 5.4.0
      */
-    public function jsonSerialize()
+    public function jsonSerialize(): mixed
     {
         return $this->call('jsonSerialize');
     }
@@ -590,19 +591,19 @@ class Coollection implements
      * The return value is cast to an integer.
      * @since 5.1.0
      */
-    public function count()
+    public function count(): int
     {
         return $this->call('count');
     }
 
     /**
      * Retrieve an external iterator
-     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
-     * @return Traversable An instance of an object implementing <b>Iterator</b> or
+     * @link https://php.net/manual/en/iteratoraggregate.getiterator.php
+     * @return Traversable<TKey, TValue>|TValue[] An instance of an object implementing <b>Iterator</b> or
      * <b>Traversable</b>
-     * @since 5.0.0
+     * @throws Exception on failure.
      */
-    public function getIterator()
+    public function getIterator(): Traversable
     {
         return $this->call('getIterator');
     }
